1da4999c9296a96d77b7a61035e50a5c65f2866f
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-11 22:12:00 +0100

    making it better

--- src/services/doubt-services.js
+++ src/services/doubt-services.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 class DoubtService {
     constructor() {
         let service = axios.create({
-            baseURL: 'https://prueba-mark-back.herokuapp.com/',
+            baseURL: 'https://prueba-mark-back.herokuapp.com/doubt/',
             // baseURL: 'http://localhost:3000/doubt'
         })
         this.service = service
